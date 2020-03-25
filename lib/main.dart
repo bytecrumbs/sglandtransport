29134a377bf44bf2e1b9099b42d6c4412b641529
Sascha Derungs <sascha.derungs@gmail.com>
2020-03-25 11:41:02 +0800

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:lta_datamall_flutter/screens/bicycle_parking.dart';
 import 'package:lta_datamall_flutter/providers/settings_provider.dart';
 import 'package:lta_datamall_flutter/screens/bus_arrivals.dart';
-import 'package:lta_datamall_flutter/screens/bus_stops.dart';
-import 'package:lta_datamall_flutter/screens/settings.dart';
+import 'package:lta_datamall_flutter/screens/main_container.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
@@ -23,75 +20,13 @@ class MyApp extends StatelessWidget {
               brightness:
                   settings.isDarkMode ? Brightness.dark : Brightness.light,
             ),
-            initialRoute: MainApp.id,
+            initialRoute: MainContainer.id,
             routes: <String, WidgetBuilder>{
-              MainApp.id: (BuildContext context) => MainApp(),
-              Settings.id: (BuildContext context) => Settings(),
+              MainContainer.id: (BuildContext context) => MainContainer(),
               BusArrivals.id: (BuildContext context) => BusArrivals(),
             },
           );
         },
-      ),
-    );
-  }
-}
-
-class MainApp extends StatefulWidget {
-  static const String id = 'main_app_screen';
-
-  @override
-  _MainAppState createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int pageIndex = 0;
-
-  List<Widget> pageList = <Widget>[
-    BusStops(),
-    BicycleParking(),
-    Settings(),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('LTA Datamall'),
-      ),
-      body: PageTransitionSwitcher(
-        transitionBuilder: (
-          Widget child,
-          Animation<double> animation,
-          Animation<double> secondaryAnimation,
-        ) {
-          return FadeThroughTransition(
-            animation: animation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          );
-        },
-        child: pageList[pageIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: pageIndex,
-        onTap: (int newValue) {
-          setState(() {
-            pageIndex = newValue;
-          });
-        },
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_bus),
-            title: const Text('Bus timing'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_bike),
-            title: const Text('Bicycle Parking'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: const Text('Settings'),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/screens/main_container.dart
@@ -0,0 +1,66 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:lta_datamall_flutter/screens/bicycle_parking.dart';
+import 'package:lta_datamall_flutter/screens/bus_stops.dart';
+import 'package:lta_datamall_flutter/screens/settings.dart';
+
+class MainContainer extends StatefulWidget {
+  static const String id = 'main_app_screen';
+
+  @override
+  _MainContainerState createState() => _MainContainerState();
+}
+
+class _MainContainerState extends State<MainContainer> {
+  int pageIndex = 0;
+
+  List<Widget> pageList = <Widget>[
+    BusStops(),
+    BicycleParking(),
+    Settings(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LTA Datamall'),
+      ),
+      body: PageTransitionSwitcher(
+        transitionBuilder: (
+          Widget child,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+        ) {
+          return FadeThroughTransition(
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          );
+        },
+        child: pageList[pageIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: (int newValue) {
+          setState(() {
+            pageIndex = newValue;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_bus),
+            title: const Text('Bus timing'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_bike),
+            title: const Text('Bicycle Parking'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: const Text('Settings'),
+          ),
+        ],
+      ),
+    );
+  }
+}
