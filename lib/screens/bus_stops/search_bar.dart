26f829f95983ea95682c493fea7f1951df8bb7c4
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-04 13:18:10 +0800

    add ability to clear textfield

--- lib/screens/bus_stops/search_bar.dart
+++ lib/screens/bus_stops/search_bar.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    final TextEditingController _controller = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: TextField(
+        controller: _controller,
         decoration: InputDecoration(
           labelText: 'Enter a Bus Stop number',
           icon: Icon(Icons.search),
-          suffixIcon: Icon(Icons.clear),
-          border: OutlineInputBorder(),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              _controller.clear();
+            },
+          ),
+          border: const OutlineInputBorder(),
         ),
       ),
     );
