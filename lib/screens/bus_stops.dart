6c57b87b7b5836e4664d4b3e260908e56de3874a
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-04 11:22:39 +0800

    refactoring

--- lib/screens/bus_stops.dart
+++ lib/screens/bus_stops.dart
@@ -5,14 +5,26 @@ import 'package:lta_datamall_flutter/api.dart';
 import 'package:lta_datamall_flutter/models/bus_stops/bus_stop_model.dart';
 import 'package:lta_datamall_flutter/screens/bus_arrivals.dart';
 
-class BusStops extends StatefulWidget {
+class BusStops extends StatelessWidget {
   static const String id = 'bus_stops_screen';
 
   @override
-  _BusStopsState createState() => _BusStopsState();
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SearchField(),
+        BusCardList(),
+      ],
+    );
+  }
 }
 
-class _BusStopsState extends State<BusStops> {
+class BusCardList extends StatefulWidget {
+  @override
+  _BusCardListState createState() => _BusCardListState();
+}
+
+class _BusCardListState extends State<BusCardList> {
   Future<List<BusStopModel>> _future;
 
   @override
@@ -23,59 +35,53 @@ class _BusStopsState extends State<BusStops> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SearchField(),
-        Expanded(
-          child: FutureBuilder<List<BusStopModel>>(
-            future: _future,
-            builder: (BuildContext context,
-                AsyncSnapshot<List<BusStopModel>> snapshot) {
-              if (snapshot.hasData) {
-                final List<BusStopModel> busStops = snapshot.data;
-                return ListView.builder(
-                  itemCount: busStops.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return OpenContainer(
-                      transitionType: ContainerTransitionType.fade,
-                      openBuilder:
-                          (BuildContext _, VoidCallback openContainer) {
-                        return BusArrivals(
-                          busStopCode: busStops[index].busStopCode,
-                          description: busStops[index].description,
-                          roadName: busStops[index].roadName,
-                        );
-                      },
-                      tappable: false,
-                      closedShape: const RoundedRectangleBorder(),
-                      closedElevation: 0.0,
-                      openColor: Theme.of(context).scaffoldBackgroundColor,
-                      closedColor: Theme.of(context).scaffoldBackgroundColor,
-                      closedBuilder:
-                          (BuildContext context, VoidCallback openContainer) {
-                        return Card(
-                          margin: const EdgeInsets.all(6),
-                          child: ListTile(
-                            leading: Icon(Icons.departure_board),
-                            onTap: openContainer,
-                            title: Text(
-                                '${busStops[index].busStopCode} (${busStops[index].description})'),
-                            subtitle: Text(busStops[index].roadName),
-                            trailing: Icon(Icons.assignment),
-                          ),
-                        );
-                      },
+    return Expanded(
+      child: FutureBuilder<List<BusStopModel>>(
+        future: _future,
+        builder:
+            (BuildContext context, AsyncSnapshot<List<BusStopModel>> snapshot) {
+          if (snapshot.hasData) {
+            final List<BusStopModel> busStops = snapshot.data;
+            return ListView.builder(
+              itemCount: busStops.length,
+              itemBuilder: (BuildContext context, int index) {
+                return OpenContainer(
+                  transitionType: ContainerTransitionType.fade,
+                  openBuilder: (BuildContext _, VoidCallback openContainer) {
+                    return BusArrivals(
+                      busStopCode: busStops[index].busStopCode,
+                      description: busStops[index].description,
+                      roadName: busStops[index].roadName,
+                    );
+                  },
+                  tappable: false,
+                  closedShape: const RoundedRectangleBorder(),
+                  closedElevation: 0.0,
+                  openColor: Theme.of(context).scaffoldBackgroundColor,
+                  closedColor: Theme.of(context).scaffoldBackgroundColor,
+                  closedBuilder:
+                      (BuildContext context, VoidCallback openContainer) {
+                    return Card(
+                      margin: const EdgeInsets.all(6),
+                      child: ListTile(
+                        leading: Icon(Icons.departure_board),
+                        onTap: openContainer,
+                        title: Text(
+                            '${busStops[index].busStopCode} (${busStops[index].description})'),
+                        subtitle: Text(busStops[index].roadName),
+                        trailing: Icon(Icons.assignment),
+                      ),
                     );
                   },
                 );
-              } else if (snapshot.hasError) {}
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-        ),
-      ],
+              },
+            );
+          } else if (snapshot.hasError) {}
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
