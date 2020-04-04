5fb07870c554938e379d92b2d1a3155138d2d539
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-04 11:18:42 +0800

    add search field

--- lib/screens/bus_stops.dart
+++ lib/screens/bus_stops.dart
@@ -23,51 +23,76 @@ class _BusStopsState extends State<BusStops> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<BusStopModel>>(
-      future: _future,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<BusStopModel>> snapshot) {
-        if (snapshot.hasData) {
-          final List<BusStopModel> busStops = snapshot.data;
-          return ListView.builder(
-            itemCount: busStops.length,
-            itemBuilder: (BuildContext context, int index) {
-              return OpenContainer(
-                transitionType: ContainerTransitionType.fade,
-                openBuilder: (BuildContext _, VoidCallback openContainer) {
-                  return BusArrivals(
-                    busStopCode: busStops[index].busStopCode,
-                    description: busStops[index].description,
-                    roadName: busStops[index].roadName,
-                  );
-                },
-                tappable: false,
-                closedShape: const RoundedRectangleBorder(),
-                closedElevation: 0.0,
-                openColor: Theme.of(context).scaffoldBackgroundColor,
-                closedColor: Theme.of(context).scaffoldBackgroundColor,
-                closedBuilder:
-                    (BuildContext context, VoidCallback openContainer) {
-                  return Card(
-                    margin: const EdgeInsets.all(6),
-                    child: ListTile(
-                      leading: Icon(Icons.departure_board),
-                      onTap: openContainer,
-                      title: Text(
-                          '${busStops[index].busStopCode} (${busStops[index].description})'),
-                      subtitle: Text(busStops[index].roadName),
-                      trailing: Icon(Icons.assignment),
-                    ),
-                  );
-                },
+    return Column(
+      children: <Widget>[
+        SearchField(),
+        Expanded(
+          child: FutureBuilder<List<BusStopModel>>(
+            future: _future,
+            builder: (BuildContext context,
+                AsyncSnapshot<List<BusStopModel>> snapshot) {
+              if (snapshot.hasData) {
+                final List<BusStopModel> busStops = snapshot.data;
+                return ListView.builder(
+                  itemCount: busStops.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return OpenContainer(
+                      transitionType: ContainerTransitionType.fade,
+                      openBuilder:
+                          (BuildContext _, VoidCallback openContainer) {
+                        return BusArrivals(
+                          busStopCode: busStops[index].busStopCode,
+                          description: busStops[index].description,
+                          roadName: busStops[index].roadName,
+                        );
+                      },
+                      tappable: false,
+                      closedShape: const RoundedRectangleBorder(),
+                      closedElevation: 0.0,
+                      openColor: Theme.of(context).scaffoldBackgroundColor,
+                      closedColor: Theme.of(context).scaffoldBackgroundColor,
+                      closedBuilder:
+                          (BuildContext context, VoidCallback openContainer) {
+                        return Card(
+                          margin: const EdgeInsets.all(6),
+                          child: ListTile(
+                            leading: Icon(Icons.departure_board),
+                            onTap: openContainer,
+                            title: Text(
+                                '${busStops[index].busStopCode} (${busStops[index].description})'),
+                            subtitle: Text(busStops[index].roadName),
+                            trailing: Icon(Icons.assignment),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {}
+              return const Center(
+                child: CircularProgressIndicator(),
               );
             },
-          );
-        } else if (snapshot.hasError) {}
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Enter a Bus Stop number',
+          icon: Icon(Icons.search),
+          suffixIcon: Icon(Icons.clear),
+          border: OutlineInputBorder(),
+        ),
+      ),
     );
   }
 }
