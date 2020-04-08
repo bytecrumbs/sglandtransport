1ede8ff34082c76b49d92b5adfac6a2ec5fac24f
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-08 21:56:32 +0800

    initial tests

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lta_datamall_flutter/models/bus_stops/bus_stop_model.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 import 'package:lta_datamall_flutter/api.dart';
@@ -44,5 +45,80 @@ dynamic main() {
     });
   });
 
-  // TODO(sascha): Add tests for fetchBusStopList
+  group('fetchBusStopList', () {
+    test(
+        'returns a List of BusStopModels if the http calls complete successfully',
+        () async {
+      final MockClient client = MockClient();
+      // TODO(sascha): Store this header value somewhere more central and reusable
+      final Map<String, String> requestHeaders = <String, String>{
+        'AccountKey': 'xNTAqVxgQiOwp9MQa9y0tQ==',
+      };
+
+      final List<int> skip = <int>[
+        0,
+        500,
+        1000,
+        1500,
+        2000,
+        2500,
+        3000,
+        3500,
+        4000,
+        4500,
+        5000,
+      ];
+
+      for (final int currentSkip in skip) {
+        // Use Mockito to return a successful response when it calls the
+        // provided http.Client.
+        when(client.get(
+          'http://datamall2.mytransport.sg/ltaodataservice/BusStops?\$skip=$currentSkip',
+          headers: requestHeaders,
+        )).thenAnswer((_) async =>
+            http.Response('{"value": [{"BusStopCode": "010201"}]}', 200));
+      }
+
+      final List<BusStopModel> busStopModelList =
+          await fetchBusStopList(client);
+
+      expect(busStopModelList, isInstanceOf<List<BusStopModel>>());
+      expect(busStopModelList.length, equals(skip.length));
+    });
+
+    test('throws an exception if any of the http calls complete with an error',
+        () {
+      final MockClient client = MockClient();
+      final Map<String, String> requestHeaders = <String, String>{
+        'AccountKey': 'xNTAqVxgQiOwp9MQa9y0tQ==',
+      };
+
+      final List<int> skip = <int>[
+        0,
+        500,
+        1000,
+        1500,
+        2000,
+        2500,
+        3000,
+        3500,
+        4000,
+        4500,
+        5000,
+      ];
+
+      for (final int currentSkip in skip) {
+        // Use Mockito to return a successful response when it calls the
+        // provided http.Client.
+        when(client.get(
+          'http://datamall2.mytransport.sg/ltaodataservice/BusStops?\$skip=$currentSkip',
+          headers: requestHeaders,
+        )).thenAnswer((_) async => http.Response(
+            '{"value": [{"BusStopCode": "010201"}]}',
+            currentSkip == skip[4] ? 404 : 200));
+      }
+
+      expect(fetchBusStopList(client), throwsException);
+    });
+  });
 }
