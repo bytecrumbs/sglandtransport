539e8e118c4ce2eb65f8d561229067358471d493
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-11 22:45:40 +0800

    add initial drawer

--- lib/screens/bus/main_container.dart
+++ lib/screens/bus/main_container.dart
@@ -25,6 +25,50 @@ class _MainContainerState extends State<MainContainer> {
       appBar: AppBar(
         title: const Text('LTA Datamall'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: const Text(
+                'Welcome to the LTA Datamall App',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).highlightColor,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.directions_bus),
+              title: const Text('Bus Timing'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.directions_bike),
+              title: const Text('Bicycle Parking'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       body: PageTransitionSwitcher(
         transitionBuilder: (
           Widget child,
