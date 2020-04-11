342eecb587436523fe8fd17df58ed0c93af266f2
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-11 08:25:23 +0800

    refactoring

--- test/screens/bus_arrivals/bus_arrival_header_test.dart
+++ test/screens/bus_arrivals/bus_arrival_header_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lta_datamall_flutter/screens/bus_arrivals/bus_arrival_header.dart';
 
-Future<void> _pumpBusStopCard(WidgetTester tester) async {
+Future<void> _pumpBusArrivalHeader(WidgetTester tester) async {
   return await tester.pumpWidget(const MaterialApp(
     home: Scaffold(
       body: BusArrivalHeader(
@@ -14,7 +14,7 @@ Future<void> _pumpBusStopCard(WidgetTester tester) async {
 
 void main() {
   testWidgets('MyWidget has a header', (WidgetTester tester) async {
-    await _pumpBusStopCard(tester);
+    await _pumpBusArrivalHeader(tester);
     final Finder headerFinder = find.text('h');
 
     expect(headerFinder, findsOneWidget);
