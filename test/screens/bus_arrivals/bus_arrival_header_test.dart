27503634678756fb63e281b8ac7a5b6c3ba0f86d
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-11 08:24:19 +0800

    initial tests

---
+++ test/screens/bus_arrivals/bus_arrival_header_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lta_datamall_flutter/screens/bus_arrivals/bus_arrival_header.dart';
+
+Future<void> _pumpBusStopCard(WidgetTester tester) async {
+  return await tester.pumpWidget(const MaterialApp(
+    home: Scaffold(
+      body: BusArrivalHeader(
+        headerText: 'h',
+      ),
+    ),
+  ));
+}
+
+void main() {
+  testWidgets('MyWidget has a header', (WidgetTester tester) async {
+    await _pumpBusStopCard(tester);
+    final Finder headerFinder = find.text('h');
+
+    expect(headerFinder, findsOneWidget);
+  });
+}
