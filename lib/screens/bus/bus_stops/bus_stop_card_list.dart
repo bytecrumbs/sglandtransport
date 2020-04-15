05402f6ee353ce3b34670e3d3b9806494b348e07
renji.raj <renji129@gmail.com>
2020-04-15 23:30:02 +0800

    #28 Minor refactor
    Move searchbar inside card list, this will help to communicate between search and list widgets

--- lib/screens/bus/bus_stops/bus_stop_card_list.dart
+++ lib/screens/bus/bus_stops/bus_stop_card_list.dart
@@ -5,6 +5,7 @@ import 'package:lta_datamall_flutter/api.dart';
 import 'package:lta_datamall_flutter/models/bus_stops/bus_stop_model.dart';
 import 'package:lta_datamall_flutter/screens/bus/bus_arrivals/bus_arrivals_screen.dart';
 import 'package:lta_datamall_flutter/screens/bus/bus_stops/bus_stop_card.dart';
+import 'package:lta_datamall_flutter/screens/bus/bus_stops/search_bar.dart';
 
 class BusStopCardList extends StatefulWidget {
   @override
@@ -30,49 +31,55 @@ class _BusStopCardListState extends State<BusStopCardList> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: FutureBuilder<List<BusStopModel>>(
-        future: _future,
-        builder:
-            (BuildContext context, AsyncSnapshot<List<BusStopModel>> snapshot) {
-          if (snapshot.hasData) {
-            final List<BusStopModel> busStops = snapshot.data;
-            return ListView.builder(
-              itemCount: busStops.length,
-              itemBuilder: (BuildContext context, int index) {
-                return OpenContainer(
-                  transitionType: ContainerTransitionType.fade,
-                  openBuilder: (BuildContext _, VoidCallback openContainer) {
-                    dismissFocus();
-                    return BusArrivalsScreen(
-                      busStopCode: busStops[index].busStopCode,
-                      description: busStops[index].description,
-                      roadName: busStops[index].roadName,
-                    );
-                  },
-                  tappable: false,
-                  closedShape: const RoundedRectangleBorder(),
-                  closedElevation: 0.0,
-                  openColor: Theme.of(context).scaffoldBackgroundColor,
-                  closedColor: Theme.of(context).scaffoldBackgroundColor,
-                  closedBuilder:
-                      (BuildContext context, VoidCallback openContainer) {
-                    return BusStopCard(
-                      openContainer: openContainer,
-                      busStopCode: busStops[index].busStopCode,
-                      description: busStops[index].description,
-                      roadName: busStops[index].roadName,
+    return Column(
+      children: <Widget>[
+        SearchBar(),
+        Expanded(
+          child: FutureBuilder<List<BusStopModel>>(
+            future: _future,
+            builder: (BuildContext context,
+                AsyncSnapshot<List<BusStopModel>> snapshot) {
+              if (snapshot.hasData) {
+                final List<BusStopModel> busStops = snapshot.data;
+                return ListView.builder(
+                  itemCount: busStops.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return OpenContainer(
+                      transitionType: ContainerTransitionType.fade,
+                      openBuilder:
+                          (BuildContext _, VoidCallback openContainer) {
+                        dismissFocus();
+                        return BusArrivalsScreen(
+                          busStopCode: busStops[index].busStopCode,
+                          description: busStops[index].description,
+                          roadName: busStops[index].roadName,
+                        );
+                      },
+                      tappable: false,
+                      closedShape: const RoundedRectangleBorder(),
+                      closedElevation: 0.0,
+                      openColor: Theme.of(context).scaffoldBackgroundColor,
+                      closedColor: Theme.of(context).scaffoldBackgroundColor,
+                      closedBuilder:
+                          (BuildContext context, VoidCallback openContainer) {
+                        return BusStopCard(
+                          openContainer: openContainer,
+                          busStopCode: busStops[index].busStopCode,
+                          description: busStops[index].description,
+                          roadName: busStops[index].roadName,
+                        );
+                      },
                     );
                   },
                 );
-              },
-            );
-          } else if (snapshot.hasError) {}
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
+              } else if (snapshot.hasError) {}
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
--- lib/screens/bus/nearby_bus_section.dart
+++ lib/screens/bus/nearby_bus_section.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/screens/bus/bus_stops/bus_stop_card_list.dart';
-import 'package:lta_datamall_flutter/screens/bus/bus_stops/search_bar.dart';
 
 class NearbyBusStops extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SearchBar(),
-        BusStopCardList(),
-      ],
+    return Container(
+      child: BusStopCardList(),
     );
   }
 }
