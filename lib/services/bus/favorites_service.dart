8f1035e3146f416768b5fc76da8f925e9f3403ea
Sascha Derungs <sascha.derungs@gmail.com>
2020-04-15 13:31:28 +0800

    change service to a singleton

--- lib/services/bus/favorites_service.dart
+++ lib/services/bus/favorites_service.dart
@@ -1,6 +1,15 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class BusFavoritesService {
+  factory BusFavoritesService() {
+    return _busFavoritesService;
+  }
+
+  BusFavoritesService._internal();
+
+  static final BusFavoritesService _busFavoritesService =
+      BusFavoritesService._internal();
+
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   Future<List<String>> getFavoriteBusStops() async {
