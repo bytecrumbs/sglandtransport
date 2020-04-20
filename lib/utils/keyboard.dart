ebe4a668252dc1bb2bdfe111e904a93b4f147fb5
Robin <ameego@gmail.com>
2020-04-20 11:12:30 +0800

    Add utility for keyboard

---
+++ lib/utils/keyboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Keyboard {
+  static void dismiss(BuildContext context) {
+    final FocusScopeNode currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+}
