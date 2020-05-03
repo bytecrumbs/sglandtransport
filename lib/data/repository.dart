4198323d500f54ed111921fa491bb463008222de
renji.raj <renji129@gmail.com>
2020-05-03 09:58:56 +0800

    #55 Removed unused code

--- lib/data/repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:lta_datamall_flutter/models/bus_stops/bus_stop_model.dart';
-
-abstract class Repository {
-  Future<List<BusStopModel>> getBusStopListBySearchText(String searchText);
-  Future<List<BusStopModel>> getBusStopList();
-}
