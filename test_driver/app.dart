ce441368272e6b0a4e90a9553a811853527ec0a0
renji.raj <renji129@gmail.com>
2020-05-16 14:33:59 +0800

    #45 Disable debug banner Add more devices in yaml

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:lta_datamall_flutter/main.dart' as app;
+import 'package:flutter/material.dart';
 
 void main() {
   // This line enables the extension.
@@ -7,5 +8,6 @@ void main() {
 
   // Call the `main()` function of the app, or call `runApp` with
   // any widget you are interested in testing.
+  WidgetsApp.debugAllowBannerOverride = false;
   app.main();
 }
