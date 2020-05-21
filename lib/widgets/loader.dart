9ca0a7bbb85abd8ac0d88b47b802778881ae3329
Robin <ameego@gmail.com>
2020-05-21 12:12:22 +0800

    Smoother transition between splash screen and bus stop list + update animation

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -2,27 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 
 class Loader extends StatelessWidget {
-  Loader({
-    this.size = 100,
-  });
-
-  final double size;
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-      ),
-      child: PhysicalModel(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(size / 2),
-        clipBehavior: Clip.antiAlias,
-        child: FlareActor('images/bus.flr',
-            alignment: Alignment.center, fit: BoxFit.contain, animation: 'Bus'),
-      ),
-    );
+    return FlareActor('images/bus.flr',
+        alignment: Alignment.center, fit: BoxFit.fitHeight, animation: 'Bus');
   }
 }
