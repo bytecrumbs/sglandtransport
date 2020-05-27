3b859efc84d897ea86674219c27f50ecc2627e82
Sascha Derungs <sascha.derungs@gmail.com>
2020-05-27 14:36:57 +0800

    store bus routes in local DB and fetch from DB

--- lib/screens/bus/bus_arrivals/bus_arrival_card_list.dart
+++ lib/screens/bus/bus_arrivals/bus_arrival_card_list.dart
@@ -1,67 +1,26 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:lta_datamall_flutter/models/bus_arrival/bus_arrival_service_model.dart';
-import 'package:lta_datamall_flutter/services/api.dart';
 import 'package:lta_datamall_flutter/models/bus_arrival/bus_arrival_model.dart';
+import 'package:lta_datamall_flutter/models/bus_arrival/bus_arrival_service_model.dart';
 import 'package:lta_datamall_flutter/screens/bus/bus_arrivals/bus_arrival_card.dart';
+import 'package:lta_datamall_flutter/services/api.dart';
 import 'package:http/io_client.dart' as http;
 
-class BusArrivalCardList extends StatefulWidget {
+class BusArrivalCardList extends StatelessWidget {
   const BusArrivalCardList({@required this.busStopCode});
 
   final String busStopCode;
 
   @override
-  _BusArrivalCardListState createState() => _BusArrivalCardListState();
-}
-
-class _BusArrivalCardListState extends State<BusArrivalCardList> {
-  Future<BusArrivalModel> _future;
-  Timer timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _future = fetchBusArrivalList(http.IOClient(), widget.busStopCode);
-    timer = Timer.periodic(
-      const Duration(minutes: 1),
-      (Timer t) => _refreshBusArrivals(),
-    );
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
-
-  Future<BusArrivalModel> _refreshBusArrivals() {
-    setState(() {
-      _future = fetchBusArrivalList(http.IOClient(), widget.busStopCode);
-    });
-
-    return _future;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<BusArrivalModel>(
-      future: _future,
-      builder: (BuildContext context, AsyncSnapshot<BusArrivalModel> snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          return snapshot.hasError
-              ? Text(snapshot.error.toString())
-              : snapshot.hasData
-                  ? RefreshIndicator(
-                      onRefresh: _refreshBusArrivals,
-                      child: _buildListView(snapshot.data.services))
-                  : Text('No Data');
+    return FutureBuilder(
+      future: fetchBusArrivalList(http.IOClient(), busStopCode),
+      builder: (context, AsyncSnapshot<BusArrivalModel> snapshot) {
+        if (snapshot.hasData) {
+          return _buildListView(snapshot.data.services);
         }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
