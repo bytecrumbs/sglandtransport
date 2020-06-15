aabe636811fb5878952cdcef60a68ebd1e0428ff
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-15 15:44:35 +0800

    add location service

--- lib/app/locator.iconfig.dart
+++ lib/app/locator.iconfig.dart
@@ -7,6 +7,7 @@
 import 'package:lta_datamall_flutter/services/api.dart';
 import 'package:lta_datamall_flutter/services/feature_service.dart';
 import 'package:lta_datamall_flutter/services/firebase_analytics_observer_service.dart';
+import 'package:lta_datamall_flutter/services/location_service.dart';
 import 'package:lta_datamall_flutter/services/thirdparty_services_module.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:get_it/get_it.dart';
@@ -17,6 +18,7 @@ void $initGetIt(GetIt g, {String environment}) {
   g.registerLazySingleton<FeatureService>(() => FeatureService());
   g.registerLazySingleton<FirebaseAnalyticsObserverService>(
       () => FirebaseAnalyticsObserverService());
+  g.registerLazySingleton<LocationService>(() => LocationService());
   g.registerLazySingleton<NavigationService>(
       () => thirdPartyServicesModule.navigationService);
 }
---
+++ lib/services/location_service.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:injectable/injectable.dart';
+import 'package:location/location.dart';
+import 'package:lta_datamall_flutter/datamodels/user_location.dart';
+
+@lazySingleton
+class LocationService {
+  Location location = Location();
+  // Continuously emit location updates
+  final StreamController<UserLocation> _locationController =
+      StreamController<UserLocation>.broadcast();
+
+  Stream<UserLocation> get locationStream => _locationController.stream;
+
+  LocationService() {
+    location.requestPermission().then((ps) {
+      if (ps == PermissionStatus.granted) {
+        location.onLocationChanged.listen((locationData) {
+          if (locationData != null) {
+            _locationController.add(
+              UserLocation(
+                latitude: locationData.latitude,
+                longitude: locationData.longitude,
+                permissionGranted: true,
+              ),
+            );
+          }
+        });
+      } else {
+        _locationController.add(
+          UserLocation(
+            latitude: null,
+            longitude: null,
+            permissionGranted: false,
+          ),
+        );
+      }
+    });
+  }
+}
