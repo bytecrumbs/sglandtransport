50071b5310537d6a0873a5596cf03a9fdc4a80f5
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-20 15:51:53 +0800

    add favourite icon to appheader

--- lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
+++ lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_arrival/bus_arrival_viewmodel.dart';
+import 'package:lta_datamall_flutter/ui/views/shared/favourites_icon/favourites_icon_view.dart';
 import 'package:stacked/stacked.dart';
 
 class BusArrivalView extends StatelessWidget {
@@ -11,6 +12,12 @@ class BusArrivalView extends StatelessWidget {
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
           title: Text('Bus Arrival'),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: FavouritesIconView(busStopCode: busStopCode),
+            ),
+          ],
         ),
         body: Center(
           child: Text(busStopCode),
