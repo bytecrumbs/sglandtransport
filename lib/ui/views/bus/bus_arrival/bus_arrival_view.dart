7165480c206757d39cd9be7872310bb40d8bc43b
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-21 20:58:47 +0800

    show bus arrival service cards

--- lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
+++ lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lta_datamall_flutter/ui/views/bus/bus_arrival/bus_arrival_viewmodel.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/favourites_icon/favourites_icon_view.dart';
 import 'package:stacked/stacked.dart';
+
+import 'bus_arrival_service_card_view.dart';
+import 'bus_arrival_viewmodel.dart';
 
 class BusArrivalView extends StatelessWidget {
   BusArrivalView({@required this.busStopCode});
@@ -22,7 +24,17 @@ class BusArrivalView extends StatelessWidget {
         body: Center(
           child: model.isBusy
               ? CircularProgressIndicator()
-              : Text(model.data.length.toString()),
+              : ListView.builder(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemCount: model.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final currentBusService = model.data[index];
+                    return BusArrivalServiceCardView(
+                      key: ValueKey<String>('busArrivalCard-$index'),
+                      busArrivalServiceModel: currentBusService,
+                    );
+                  },
+                ),
         ),
       ),
       viewModelBuilder: () => BusArrivalViewModel(busStopCode),
