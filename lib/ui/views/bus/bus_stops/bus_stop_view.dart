9e8e8e4c749448f6cf4eb7e319386fb98c4641ec
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-22 15:02:32 +0800

    add navigation to Bus Arrival screen

--- lib/ui/views/bus/bus_stops/bus_stop_view.dart
+++ lib/ui/views/bus/bus_stops/bus_stop_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/datamodels/bus/bus_stop/bus_stop_model.dart';
+import 'package:lta_datamall_flutter/ui/views/bus/bus_stops/bus_stop_view_model.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/box_info/box_info_view.dart';
+import 'package:stacked/stacked.dart';
 
 class BusStopView extends StatelessWidget {
   const BusStopView({
@@ -12,16 +14,22 @@ class BusStopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(6),
-      child: ListTile(
-        leading: Icon(Icons.departure_board),
-        title: Text(busStopModel.description),
-        subtitle:
-            Text('${busStopModel.busStopCode} | ${busStopModel.roadName}'),
-        trailing: _buildBoxInfo(context, busStopModel.distanceInMeters),
-        onTap: () {},
+    return ViewModelBuilder<BusStopViewModel>.nonReactive(
+      builder: (context, model, child) => Card(
+        margin: const EdgeInsets.all(6),
+        child: ListTile(
+          leading: Icon(Icons.departure_board),
+          title: Text(busStopModel.description),
+          subtitle:
+              Text('${busStopModel.busStopCode} | ${busStopModel.roadName}'),
+          trailing: _buildBoxInfo(context, busStopModel.distanceInMeters),
+          onTap: () {
+            model.navigateToBusArrival(
+                busStopModel.busStopCode, busStopModel.description);
+          },
+        ),
       ),
+      viewModelBuilder: () => BusStopViewModel(),
     );
   }
 
