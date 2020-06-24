0a48f943814ff73cd5cded9731b74f6a2a82247c
Renji <renji129@gmail.com>
2020-06-24 22:08:25 +0800

    Removed userLoaction from member variable

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -5,28 +5,31 @@ import 'package:lta_datamall_flutter/datamodels/user_location.dart';
 @lazySingleton
 class LocationService {
   final Location _location = Location();
-  UserLocation _userLocation = UserLocation(
-    latitude: 0,
-    longitude: 0,
-    permissionGranted: false,
-  );
 
   Future<UserLocation> getUserLocation() async {
+    var userLocation = UserLocation(
+      latitude: 0,
+      longitude: 0,
+      permissionGranted: false,
+    );
+
+    await _location.changeSettings(accuracy: LocationAccuracy.low);
+
     var _permissionGranted = await _location.hasPermission();
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await _location.requestPermission();
       if (_permissionGranted != PermissionStatus.granted) {
-        return _userLocation;
+        return userLocation;
       }
     }
 
     var locationData = await _location.getLocation();
-    _userLocation = UserLocation(
+    userLocation = UserLocation(
       latitude: locationData.latitude,
       longitude: locationData.longitude,
       permissionGranted: true,
     );
 
-    return _userLocation;
+    return userLocation;
   }
 }
