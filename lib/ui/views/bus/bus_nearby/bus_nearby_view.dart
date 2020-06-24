a7f130d8db8a5f68997ed1e0b74edaa00ba2fe83
renji.raj <renji129@gmail.com>
2020-06-24 14:44:11 +0800

    Removed bus stops view

--- lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
+++ lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_stops/bus_stop_view.dart';
-import 'package:lta_datamall_flutter/ui/views/bus/bus_stops/bus_stops_view.dart';
 import 'package:stacked/stacked.dart';
 
 import 'bus_nearby_viewmodel.dart';
