98af316cada22cde7ccbc937ff7b2f7bab9fc91a
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-26 21:42:27 +0800

    fix tests

--- test/viewmodel_tests/bus_viewmodel_test.dart
+++ test/viewmodel_tests/bus_viewmodel_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_viewmodel.dart';
 
+import '../setup/test_helpers.dart';
+
 void main() {
-  group('BusViewModelTest', () {
-    group('currentIndex', () {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group('BusViewModelTest -', () {
+    setUp(() => registerServices());
+    tearDown(() => unregisterServices());
+    group('currentIndex -', () {
       test('When constructed it is 0', () {
         var model = BusViewModel();
         expect(model.currentIndex, 0);
