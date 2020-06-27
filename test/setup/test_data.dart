d3f40793738212b90ba26c162471d0cd5e631676
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-27 10:43:54 +0800

    tests for BusArrivalViewModel

---
+++ test/setup/test_data.dart
@@ -0,0 +1,6 @@
+import 'package:lta_datamall_flutter/datamodels/bus/bus_arrival/bus_arrival_service_model.dart';
+
+List<BusArrivalServiceModel> busArrivalServiceModelList = [
+  BusArrivalServiceModel(serviceNo: '12'),
+  BusArrivalServiceModel(serviceNo: '130'),
+];
--- test/setup/test_helpers.dart
+++ test/setup/test_helpers.dart
@@ -1,5 +1,25 @@
 import 'package:lta_datamall_flutter/app/locator.dart';
+import 'package:lta_datamall_flutter/services/bus_service.dart';
 import 'package:lta_datamall_flutter/services/firebase_analytics_service.dart';
+import 'package:mockito/mockito.dart';
+
+import 'test_data.dart' as test_data;
+
+class BusServiceMock extends Mock implements BusService {}
+
+BusService getAndRegisterBusServiceMock({
+  String busStopForBusArrival = '01019',
+}) {
+  _removeRegistrationIfExists<BusService>();
+  var service = BusServiceMock();
+  when(service.getBusArrivalServices(busStopForBusArrival)).thenAnswer(
+    (realInvocation) {
+      return Future.value(test_data.busArrivalServiceModelList);
+    },
+  );
+  locator.registerSingleton<BusService>(service);
+  return service;
+}
 
 FirebaseAnalyticsService getAndRegisterFirebaseAnalyticsService() {
   _removeRegistrationIfExists<FirebaseAnalyticsService>();
@@ -10,10 +30,12 @@ FirebaseAnalyticsService getAndRegisterFirebaseAnalyticsService() {
 
 void registerServices() {
   getAndRegisterFirebaseAnalyticsService();
+  getAndRegisterBusServiceMock();
 }
 
 void unregisterServices() {
   locator.unregister<FirebaseAnalyticsService>();
+  locator.unregister<BusService>();
 }
 
 void _removeRegistrationIfExists<T>() {
