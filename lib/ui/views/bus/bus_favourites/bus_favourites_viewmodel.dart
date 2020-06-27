ef8619856476dd1419052a79ac0aa81bfb4304ee
Sascha Derungs <sascha.derungs@gmail.com>
2020-06-27 19:18:15 +0800

    remove unused code

--- lib/ui/views/bus/bus_favourites/bus_favourites_viewmodel.dart
+++ lib/ui/views/bus/bus_favourites/bus_favourites_viewmodel.dart
@@ -2,13 +2,11 @@ import 'package:lta_datamall_flutter/app/locator.dart';
 import 'package:lta_datamall_flutter/app/router.gr.dart';
 import 'package:lta_datamall_flutter/datamodels/bus/bus_stop/bus_stop_model.dart';
 import 'package:lta_datamall_flutter/services/bus_service.dart';
-import 'package:lta_datamall_flutter/services/favourites_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class BusFavouritesViewModel extends ReactiveViewModel {
   final _navigationService = locator<NavigationService>();
-  final _favouritesService = locator<FavouritesService>();
   final _busService = locator<BusService>();
 
   List<BusStopModel> get favouriteBusStops => _busService.favouriteBusStops;
@@ -19,12 +17,6 @@ class BusFavouritesViewModel extends ReactiveViewModel {
   Future<void> initialize() async {
     await _busService.setFavouriteBusStops();
   }
-
-  Future<List<String>> addBusStop(String busStopCode) =>
-      _favouritesService.addBusStop(busStopCode);
-
-  Future<List<String>> removeBusStop(String busStopCode) =>
-      _favouritesService.removeBusStop(busStopCode);
 
   Future navigateToBusArrival(String busStopCode, String description) async {
     await _navigationService.navigateTo(Routes.busArrivalView,
