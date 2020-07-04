9c21b1daa6f9dfcd6196b806ac76339e570a6465
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-04 18:03:01 +0800

    remove unnecessary container

--- lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
+++ lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
@@ -12,25 +12,23 @@ class BusNearbyView extends StatelessWidget {
       builder: (context, model, child) =>
           model.isBusy || model.nearByBusStopList.isEmpty
               ? Center(child: CircularProgressIndicator())
-              : Container(
-                  child: ListView.builder(
-                    itemCount: model.nearByBusStopList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return AnimationConfiguration.staggeredList(
-                        position: index,
-                        duration: const Duration(milliseconds: 375),
-                        child: SlideAnimation(
-                          verticalOffset: 50.0,
-                          child: FadeInAnimation(
-                            child: BusStopView(
-                              busStopModel: model.nearByBusStopList[index],
-                              key: ValueKey<String>('busStopCard-$index'),
-                            ),
+              : ListView.builder(
+                  itemCount: model.nearByBusStopList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return AnimationConfiguration.staggeredList(
+                      position: index,
+                      duration: const Duration(milliseconds: 375),
+                      child: SlideAnimation(
+                        verticalOffset: 50.0,
+                        child: FadeInAnimation(
+                          child: BusStopView(
+                            busStopModel: model.nearByBusStopList[index],
+                            key: ValueKey<String>('busStopCard-$index'),
                           ),
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
       viewModelBuilder: () => BusNearByViewModel(),
     );
