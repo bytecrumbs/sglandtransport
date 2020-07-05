f8f4a48862f50ce4edbb948c7014b25925e479af
Robin <ameego@gmail.com>
2020-07-05 11:54:38 +0800

    Move sliver to nearbyview, fix animation alignment, scroll behaviour when less number of items are shown on the screen

--- lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
+++ lib/ui/views/bus/bus_nearby/bus_nearby_view.dart
@@ -1,35 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_stop/bus_stop_view.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_cache_builder.dart';
+import 'package:flutter/services.dart';
+import 'package:flare_flutter/provider/asset_flare.dart';
 
 import 'bus_nearby_viewmodel.dart';
 
 class BusNearbyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final asset = AssetFlare(bundle: rootBundle, name: 'images/city.flr');
+
     return ViewModelBuilder<BusNearByViewModel>.reactive(
-      builder: (context, model, child) =>
-          model.isBusy || model.nearByBusStopList.isEmpty
-              ? Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  itemCount: model.nearByBusStopList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return AnimationConfiguration.staggeredList(
-                      position: index,
-                      duration: const Duration(milliseconds: 375),
-                      child: SlideAnimation(
-                        verticalOffset: 50.0,
-                        child: FadeInAnimation(
-                          child: BusStopView(
-                            busStopModel: model.nearByBusStopList[index],
-                            key: ValueKey<String>('busStopCard-$index'),
+      builder: (context, model, child) => Container(
+        child: model.isBusy || model.nearByBusStopList.isEmpty
+            ? Center(child: CircularProgressIndicator())
+            : CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    title: Text('Nearby Buses'),
+                    pinned: true,
+                    floating: false,
+                    snap: false,
+                    expandedHeight: 290.0,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Container(
+                        height: 320,
+                        child: FlareCacheBuilder(
+                          [asset],
+                          builder: (BuildContext context, bool isWarm) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 320,
+                              child: FlareActor.asset(
+                                asset,
+                                alignment: Alignment.center,
+                                fit: BoxFit.cover,
+                                animation: 'Loop',
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      List.generate(
+                        model.nearByBusStopList.length,
+                        (index) => AnimationConfiguration.staggeredList(
+                          position: index,
+                          duration: const Duration(milliseconds: 200),
+                          child: SlideAnimation(
+                            verticalOffset: 20.0,
+                            child: FadeInAnimation(
+                              child: BusStopView(
+                                busStopModel: model.nearByBusStopList[index],
+                                key: ValueKey<String>('busStopCard-$index'),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                ],
+              ),
+      ),
       viewModelBuilder: () => BusNearByViewModel(),
     );
   }
--- lib/ui/views/bus/bus_view.dart
+++ lib/ui/views/bus/bus_view.dart
@@ -1,9 +1,5 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-import 'package:flare_flutter/flare_actor.dart';
-import 'package:flare_flutter/flare_cache_builder.dart';
-import 'package:flare_flutter/provider/asset_flare.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:lta_datamall_flutter/ui/views/app_drawer/app_drawer_view.dart';
 import 'package:stacked/stacked.dart';
 
@@ -25,46 +21,11 @@ class BusView extends StatelessWidget {
       builder: (context, model, child) => Scaffold(
         extendBodyBehindAppBar: true,
         backgroundColor: Color.fromRGBO(212, 238, 249, 1),
+        // appBar: AppBar(
+        //   title: const Text('Buses'),
+        // ),
         drawer: AppDrawerView(),
-        body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            final asset =
-                AssetFlare(bundle: rootBundle, name: 'images/city.flr');
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text('Buses',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Container(
-                    child: FlareCacheBuilder(
-                      [asset],
-                      builder: (BuildContext context, bool isWarm) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 320,
-                          child: FlareActor.asset(
-                            asset,
-                            alignment: Alignment.center,
-                            fit: BoxFit.fitHeight,
-                            animation: 'Loop',
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: _pageList[model.currentIndex],
-        ),
+        body: _pageList[model.currentIndex],
         bottomNavigationBar: ConvexAppBar(
           key: Key('BottomBar'),
           color: Colors.grey,
