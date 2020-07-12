52479a1896f2491592fe2755363fa6056cf20cc9
Renji <renji129@gmail.com>
2020-07-12 21:56:32 +0800

    #83 Fixed animation issue

--- lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
+++ lib/ui/views/bus/bus_arrival/bus_arrival_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/favourites_icon/favourites_icon_view.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/staggered_animation/staggered_animation.dart';
 import 'package:stacked/stacked.dart';
@@ -32,21 +33,23 @@ class BusArrivalView extends StatelessWidget {
         body: Center(
           child: model.busArrivalList.isEmpty
               ? CircularProgressIndicator()
-              : Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                  child: ListView.builder(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemCount: model.busArrivalList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final currentBusService = model.busArrivalList[index];
-                      return StaggeredAnimation(
-                        index: index,
-                        child: BusArrivalServiceCardView(
-                          key: ValueKey<String>('busArrivalCard-$index'),
-                          busArrivalServiceModel: currentBusService,
-                        ),
-                      );
-                    },
+              : AnimationLimiter(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    child: ListView.builder(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      itemCount: model.busArrivalList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final currentBusService = model.busArrivalList[index];
+                        return StaggeredAnimation(
+                          index: index,
+                          child: BusArrivalServiceCardView(
+                            key: ValueKey<String>('busArrivalCard-$index'),
+                            busArrivalServiceModel: currentBusService,
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
         ),
