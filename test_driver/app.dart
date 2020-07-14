046315032cf6f0ea905242c3d427c727fa050d80
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-14 14:42:04 +0800

    adding some integration tests

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,8 +1,12 @@
+import 'package:flare_flutter/flare_testing.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:lta_datamall_flutter/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
 
+  WidgetsApp.debugAllowBannerOverride = false;
+  FlareTesting.setup();
   app.main();
 }
---
+++ test_driver/app_test.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final bundle = 'com.saschaderungs.ltaDatamall';
+  group('SG Land Transport App', () {
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        await driver.close();
+      }
+    });
+
+    group('Buses', () {
+      test('Should load the app', () async {
+        await driver.runUnsynchronized(() async {
+          await driver.waitFor(find.byValueKey('BottomBar'));
+          await driver.tap(find.text('Buses'));
+        });
+      });
+
+      test('Should switch between Nearby and Favorites', () async {
+        await driver.runUnsynchronized(() async {
+          await driver.waitFor(find.byValueKey('BottomBar'));
+          await driver.tap(find.text('Nearby'));
+          await driver.tap(find.text('Favorites'));
+          await driver.tap(find.text('Nearby'));
+        });
+      });
+
+      test('Should show bus arrival times for a given bus stop', () {});
+
+      test('Should add a bus stop to Favorites', () {});
+
+      test('Should remove a bus stop from Favorites', () {});
+
+      test('Should open Search screen', () {});
+
+      test('Should show a search result', () {});
+    });
+  });
+}
