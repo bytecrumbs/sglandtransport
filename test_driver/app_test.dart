d182e7ee39b693091a0153dcc4bf52f81ce4aec9
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-14 14:42:27 +0800

    remove unused imports

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final bundle = 'com.saschaderungs.ltaDatamall';
   group('SG Land Transport App', () {
     FlutterDriver driver;
 
