56a745824a84f1a392c941f10af8cbd40960e45c
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-16 10:19:37 +0800

    hardcode a location when service is used from a UI test

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -4,6 +4,7 @@ import 'package:injectable/injectable.dart';
 import 'package:location/location.dart';
 import 'package:logging/logging.dart';
 import 'package:lta_datamall_flutter/datamodels/user_location.dart';
+import 'package:lta_datamall_flutter/environment_config.dart';
 
 @lazySingleton
 class LocationService {
@@ -21,40 +22,54 @@ class LocationService {
     bool _serviceEnabled;
     PermissionStatus _permissionGranted;
 
-    _serviceEnabled = await _location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await _location.requestService();
+    // If a UI test is run, do not use a location stream, but use a hardcoded
+    // latitude and altitude
+    if (EnvironmentConfig.is_flutter_drive_run) {
+      _locationController.add(
+        UserLocation(
+          latitude: 1.29685,
+          longitude: 103.853,
+          permissionGranted: true,
+        ),
+      );
+    } else {
+      _serviceEnabled = await _location.serviceEnabled();
       if (!_serviceEnabled) {
-        return;
+        _serviceEnabled = await _location.requestService();
+        if (!_serviceEnabled) {
+          return;
+        }
       }
+
+      _permissionGranted = await _location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await _location.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+      _log.info('Start to listen to location stream');
+
+      _locationSubscription =
+          _location.onLocationChanged.handleError((dynamic err) {
+        _log.severe('Error in location Stream: ${err.code}');
+        _locationSubscription.cancel();
+      }).listen(
+        (locationData) {
+          _log.info('Getting a new location');
+          if (locationData != null) {
+            _locationController.add(
+              UserLocation(
+                latitude: locationData.latitude,
+                longitude: locationData.longitude,
+                permissionGranted: true,
+              ),
+            );
+          }
+        },
+      );
     }
-
-    _permissionGranted = await _location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await _location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    _log.info('Start to listen to location stream');
-
-    _locationSubscription =
-        _location.onLocationChanged.handleError((dynamic err) {
-      _log.severe('Error in location Stream: ${err.code}');
-      _locationSubscription.cancel();
-    }).listen((locationData) {
-      _log.info('Getting a new location');
-      if (locationData != null) {
-        _locationController.add(
-          UserLocation(
-            latitude: locationData.latitude,
-            longitude: locationData.longitude,
-            permissionGranted: true,
-          ),
-        );
-      }
-    });
   }
 
   Future<void> cancelLocationStream() async {
