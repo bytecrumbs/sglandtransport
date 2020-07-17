29ba78c16afbed18054f4616ce74f970638fe93b
Robin <ameego@gmail.com>
2020-07-17 09:55:43 +0800

    Fix overflow issue in smaller device for bus arrivals

--- lib/ui/views/shared/tag/tag.dart
+++ lib/ui/views/shared/tag/tag.dart
@@ -3,27 +3,46 @@ import 'package:flutter/material.dart';
 class Tag extends StatelessWidget {
   Tag({@required this.text});
   final String text;
-  @override
-  Widget build(BuildContext context) {
+
+  Widget _displayTagForSmallDevice(context) {
     return Container(
       margin: const EdgeInsets.only(right: 10),
-      padding: const EdgeInsets.only(
-        left: 5,
-        right: 5,
-        top: 5,
-        bottom: 5,
-      ),
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(244, 247, 248, 1),
-        borderRadius: BorderRadius.circular(6),
-      ),
       child: Text(
         text,
         style: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
+          fontSize: 12,
+          fontWeight: FontWeight.w300,
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isSmall = screenWidth < 340;
+    print(isSmall);
+    return isSmall
+        ? _displayTagForSmallDevice(context)
+        : Container(
+            margin: const EdgeInsets.only(right: 10),
+            padding: const EdgeInsets.only(
+              left: 5,
+              right: 5,
+              top: 5,
+              bottom: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(244, 247, 248, 1),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          );
   }
 }
