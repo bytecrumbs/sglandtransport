0ec4a9faa933848756cc9067a6254ad0cbd67015
Robin <ameego@gmail.com>
2020-07-17 23:59:44 +0800

    Remove extra padding at top + add animation limiter to favorites

--- lib/ui/views/bus/bus_favourites/bus_favourites_view.dart
+++ lib/ui/views/bus/bus_favourites/bus_favourites_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_stop/bus_stop_view.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/staggered_animation/staggered_animation.dart';
 import 'package:stacked/stacked.dart';
 import 'bus_favourites_viewmodel.dart';
@@ -20,19 +21,25 @@ class BusFavouritesView extends StatelessWidget {
                   ),
                 ),
               )
-            : ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: model.favouriteBusStops.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return StaggeredAnimation(
-                    index: index,
-                    child: BusStopView(
-                      busStopModel: model.favouriteBusStops[index],
-                      key: ValueKey<String>('busStopCard-$index'),
-                    ),
-                  );
-                },
+            : AnimationLimiter(
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.favouriteBusStops.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return StaggeredAnimation(
+                        index: index,
+                        child: BusStopView(
+                          busStopModel: model.favouriteBusStops[index],
+                          key: ValueKey<String>('busStopCard-$index'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
       ),
       viewModelBuilder: () => BusFavouritesViewModel(),
