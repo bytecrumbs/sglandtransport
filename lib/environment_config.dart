5c3a8a2edced61ff90c1920795c890a21cc7fab9
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-18 21:32:33 +0800

    hardcode ui test flag to false (must revert again!)

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,6 +1,3 @@
 class EnvironmentConfig {
-  static const is_flutter_drive_run =
-      bool.hasEnvironment('IS_FLUTTER_DRIVE_RUN')
-          ? String.fromEnvironment('IS_FLUTTER_DRIVE_RUN') == 'TRUE'
-          : false;
+  static const is_flutter_drive_run = false;
 }
