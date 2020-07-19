d0ee9628c307adef7806d1a5a49ffa4eeb281fe2
Sascha Derungs <sascha.derungs@gmail.com>
2020-07-19 09:00:34 +0800

    testing new environment variable

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,4 +1,5 @@
 class EnvironmentConfig {
   static const is_flutter_drive_run =
-      bool.fromEnvironment('IS_FLUTTER_DRIVE_RUN', defaultValue: false) == true;
+      String.fromEnvironment('IS_FLUTTER_DRIVE_RUN', defaultValue: 'false') ==
+          'newValue';
 }
