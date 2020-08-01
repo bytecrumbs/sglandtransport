430b649e3ba0155187431fd670f4e244e50cf682
Sascha Derungs <sascha.derungs@gmail.com>
2020-08-01 15:14:23 +0800

    add initial version of "rate my app" feature

--- lib/ui/views/bus/bus_view.dart
+++ lib/ui/views/bus/bus_view.dart
@@ -2,6 +2,7 @@ import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:lta_datamall_flutter/ui/views/app_drawer/app_drawer_view.dart';
 import 'package:lta_datamall_flutter/ui/views/shared/sliver_view/sliver_view.dart';
+import 'package:rate_my_app/rate_my_app.dart';
 import 'package:stacked/stacked.dart';
 
 import 'bus_favourites/bus_favourites_view.dart';
@@ -22,36 +23,60 @@ class BusView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<BusViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        drawer: AppDrawerView(),
-        body: SliverView(
-          title: model.appBarTitle,
-          child: _getViewForIndex(model.currentIndex),
+    return RateMyAppBuilder(
+      builder: (context) => ViewModelBuilder<BusViewModel>.reactive(
+        builder: (context, model, child) => Scaffold(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          drawer: AppDrawerView(),
+          body: SliverView(
+            title: model.appBarTitle,
+            child: _getViewForIndex(model.currentIndex),
+          ),
+          bottomNavigationBar: ConvexAppBar(
+            key: Key('BottomBar'),
+            color: Theme.of(context).primaryColorDark,
+            activeColor: Theme.of(context).accentColor,
+            backgroundColor: Theme.of(context).bottomAppBarColor,
+            top: -25.0,
+            style: TabStyle.react,
+            initialActiveIndex: model.currentIndex,
+            onTap: model.setIndex,
+            items: [
+              TabItem(
+                icon: Icons.location_searching,
+                title: 'Nearby',
+              ),
+              TabItem(
+                icon: Icons.favorite,
+                title: 'Favorites',
+              )
+            ],
+          ),
         ),
-        bottomNavigationBar: ConvexAppBar(
-          key: Key('BottomBar'),
-          color: Theme.of(context).primaryColorDark,
-          activeColor: Theme.of(context).accentColor,
-          backgroundColor: Theme.of(context).bottomAppBarColor,
-          top: -25.0,
-          style: TabStyle.react,
-          initialActiveIndex: model.currentIndex,
-          onTap: model.setIndex,
-          items: [
-            TabItem(
-              icon: Icons.location_searching,
-              title: 'Nearby',
-            ),
-            TabItem(
-              icon: Icons.favorite,
-              title: 'Favorites',
-            )
-          ],
-        ),
+        viewModelBuilder: () => BusViewModel(),
       ),
-      viewModelBuilder: () => BusViewModel(),
+      rateMyApp: RateMyApp(
+        preferencesPrefix: 'rateMyApp_',
+        minDays: 7,
+        minLaunches: 10,
+        remindDays: 7,
+        remindLaunches: 10,
+      ),
+      onInitialized: (context, rateMyApp) {
+        rateMyApp.conditions.forEach((condition) {
+          if (condition is DebuggableCondition) {
+            print(condition
+                .valuesAsString); // We iterate through our list of conditions and we print all debuggable ones.
+          }
+        });
+
+        print('Are all conditions met ? ' +
+            (rateMyApp.shouldOpenDialog ? 'Yes' : 'No'));
+
+        if (rateMyApp.shouldOpenDialog) {
+          rateMyApp.showRateDialog(context);
+        }
+      },
     );
   }
 }
