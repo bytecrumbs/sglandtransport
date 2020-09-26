ea0dc84110151a55c5e3d2b353d8b307599f4e97
Sascha Derungs <sascha.derungs@gmail.com>
2020-09-26 08:50:16 +0800

    remove all in app purchase related changes

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,11 +1,9 @@
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_arrival/bus_arrival_view.dart';
 import 'package:lta_datamall_flutter/ui/views/bus/bus_view.dart';
-import 'package:lta_datamall_flutter/ui/views/iap/purchase_view.dart';
 
 @MaterialAutoRouter(routes: [
   MaterialRoute(page: BusView, initial: true),
   MaterialRoute(page: BusArrivalView),
-  MaterialRoute(page: PurchaseView),
 ])
 class $Router {}
