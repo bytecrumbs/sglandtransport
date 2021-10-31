bb661115e6ea27b6353b7121f3573f586497123c
Sascha Derungs <sascha.derungs@gmail.com>
2021-10-31 12:05:07 +0800

    initial bus arrival card layout

--- lib/src/bus_stop/widgets/bus_arrival_card.dart
+++ lib/src/bus_stop/widgets/bus_arrival_card.dart
@@ -12,17 +12,66 @@ class BusArrivalCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(busArrivalModel.serviceNo),
-        subtitle: Column(
-          children: [
-            Text(busArrivalModel.nextBus.estimatedArrivalAbsolute ?? ''),
-            Text(busArrivalModel.nextBus.getEstimatedArrival()),
-            Text(busArrivalModel.nextBus.getLoadLongDescription()),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(busArrivalModel.serviceNo),
+              const SizedBox(width: 10),
+              Text(busArrivalModel.nextBus.destinationCode ?? ''),
+            ],
+          ),
+          if (busArrivalModel.inService)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _NextBusDetails(
+                  estimatedArrival:
+                      busArrivalModel.nextBus.getEstimatedArrival(),
+                  loadDescription:
+                      busArrivalModel.nextBus.getLoadLongDescription(),
+                ),
+                _NextBusDetails(
+                  estimatedArrival:
+                      busArrivalModel.nextBus2.getEstimatedArrival(),
+                  loadDescription:
+                      busArrivalModel.nextBus2.getLoadLongDescription(),
+                ),
+                _NextBusDetails(
+                  estimatedArrival:
+                      busArrivalModel.nextBus3.getEstimatedArrival(),
+                  loadDescription:
+                      busArrivalModel.nextBus3.getLoadLongDescription(),
+                ),
+              ],
+            )
+          else
+            const Text('Not in Operation')
+        ],
       ),
+    );
+  }
+}
+
+class _NextBusDetails extends StatelessWidget {
+  const _NextBusDetails({
+    Key? key,
+    required this.estimatedArrival,
+    required this.loadDescription,
+  }) : super(key: key);
+
+  final String estimatedArrival;
+  final String loadDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(estimatedArrival),
+        Text(loadDescription),
+      ],
     );
   }
 }
