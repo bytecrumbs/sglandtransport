52687229695deca4d2c8294c4db343c731b9efa6
Sascha Derungs <sascha.derungs@gmail.com>
2022-01-22 21:13:10 +0800

    fix issue when a favorite bus service is not in service at the moment

--- lib/src/bus/widgets/bus_stop_list_favorites_view_model.dart
+++ lib/src/bus/widgets/bus_stop_list_favorites_view_model.dart
@@ -25,12 +25,12 @@ class BusStopListFavoritesViewModel
   late final Timer _timer;
 
   Future<void> init() async {
-    state = AsyncValue.data(await getFavoriteBusServiceNo());
+    state = AsyncValue.data(await getFavoriteBusServices());
     _read(loggerProvider).d('starting timer for favorite bus arrival refresh');
     _timer = Timer.periodic(
       busArrivalRefreshDuration,
       (_) async {
-        state = AsyncValue.data(await getFavoriteBusServiceNo());
+        state = AsyncValue.data(await getFavoriteBusServices());
       },
     );
   }
@@ -42,33 +42,46 @@ class BusStopListFavoritesViewModel
     super.dispose();
   }
 
-  Future<List<BusArrivalServicesModel>> getFavoriteBusServiceNo() async {
+  Future<List<BusArrivalServicesModel>> getFavoriteBusServices() async {
     final localStorageService = _read(localStorageServiceProvider);
     final currentFavorites =
         localStorageService.getStringList(favoriteServiceNoKey);
     final repository = _read(busRepositoryProvider);
 
-    // execute all async calls of bus service no favorites in parallel
-    final futures = await Future.wait<List<BusArrivalServicesModel>>(
-      currentFavorites.map(
-        (e) {
-          final busStopCode = e.split(busStopCodeServiceNoDelimiter)[0];
-          final serviceNo = e.split(busStopCodeServiceNoDelimiter)[1];
-          return repository.fetchBusArrivals(
-            busStopCode: busStopCode,
+    final busServices = <BusArrivalServicesModel>[];
+
+    for (final favorite in currentFavorites) {
+      final busStopCode = favorite.split(busStopCodeServiceNoDelimiter)[0];
+      final serviceNo = favorite.split(busStopCodeServiceNoDelimiter)[1];
+      final busArrivalServicesModelList = await repository.fetchBusArrivals(
+        busStopCode: busStopCode,
+        serviceNo: serviceNo,
+      );
+
+      // if the bus is not in service, the API will return an empty list
+      if (busArrivalServicesModelList.isEmpty) {
+        busServices.add(
+          BusArrivalServicesModel(
             serviceNo: serviceNo,
-          );
-        },
-      ),
-    );
+            busOperator: '',
+            nextBus: NextBusModel(),
+            nextBus2: NextBusModel(),
+            nextBus3: NextBusModel(),
+            inService: false,
+          ),
+        );
+      } else {
+        busServices.add(busArrivalServicesModelList[0]);
+      }
+    }
 
     // as [fetchBusArrivals] returns a list, but that list will only have 1
     // entry because we are fetching based on bus service no, we have to remove
     // the inner list.
-    final busServiceNoList = futures.map((e) => e[0]).toList();
+    // final busServiceNoList = futures.map((e) => e[0]).toList();
 
     final busServiceNoListWithDestination =
-        await _addDestination(busArrivals: busServiceNoList);
+        await _addDestination(busArrivals: busServices);
 
     // Mark all entires as favorite bus services
     final busArrivalsWithFavorites = busServiceNoListWithDestination
