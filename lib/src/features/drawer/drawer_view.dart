5c3b1f325283562a03857b94517448ee1f16bf55
Sascha Derungs <sascha.derungs@gmail.com>
2022-10-09 11:18:12 +0800

    add manual rate option

--- lib/src/features/drawer/drawer_view.dart
+++ lib/src/features/drawer/drawer_view.dart
@@ -1,16 +1,20 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:share_plus/share_plus.dart';
 
 import '../../constants/environment_config.dart';
 import '../../features/about/about_view.dart';
+import '../../shared/services/rate_app_service.dart';
 
-class DrawerView extends StatelessWidget {
+class DrawerView extends ConsumerWidget {
   const DrawerView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final rateAppService = ref.watch(rateAppServiceProvider);
+
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -39,6 +43,13 @@ class DrawerView extends StatelessWidget {
                 'Check out SG Land Transport here: https://sglandtransport.app',
                 subject: 'SG Land Transport',
               );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.rate_review),
+            title: const Text('Rate SG Land Transport'),
+            onTap: () async {
+              await rateAppService.requestReview(force: true);
             },
           ),
         ],
