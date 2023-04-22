51121903d67a9d6f60061e29a15185cf3492cd86
Sascha Derungs <sascha.derungs@gmail.com>
2023-04-22 18:08:58 +0800

    mock firebase for tests

--- test/src/firebase_mocks.dart
+++ test/src/firebase_mocks.dart
@@ -1,4 +1,3 @@
-// ignore: depend_on_referenced_packages
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,7 +11,6 @@ void setupFirebaseAuthMocks([Callback? customHandlers]) {
 }
 
 Future<T> neverEndingFuture<T>() async {
-  // ignore: literal_only_boolean_expressions
   while (true) {
     // ignore: inference_failure_on_instance_creation
     await Future.delayed(const Duration(minutes: 5));
