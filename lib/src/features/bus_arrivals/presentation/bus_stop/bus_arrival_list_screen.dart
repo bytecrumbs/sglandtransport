c1a9dd1b3bd7a74f4052159b4663777571223e10
Renji <renji129@gmail.com>
2023-04-27 21:51:22 +0800

    Add refresh counter

--- lib/src/features/bus_arrivals/presentation/bus_stop/bus_arrival_list_screen.dart
+++ lib/src/features/bus_arrivals/presentation/bus_stop/bus_arrival_list_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../bus_stops/presentation/bus_stop_card/bus_stop_card_with_fetch.dart';
 import 'bus_arrival_list.dart';
 import 'bus_load_legend.dart';
 
-class BusArrivalsListScreen extends StatelessWidget {
+final timerProvider = StreamProvider<int>((ref) {
+  const initialCountdown = 10;
+  return Stream<int>.periodic(
+    const Duration(seconds: 1),
+    (count) => initialCountdown - count,
+  ).takeWhile((value) => value >= 0);
+});
+
+class BusArrivalsListScreen extends ConsumerWidget {
   const BusArrivalsListScreen({
     super.key,
     required this.busStopCode,
@@ -13,19 +22,31 @@ class BusArrivalsListScreen extends StatelessWidget {
   final String busStopCode;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final timer = ref.watch(timerProvider).value ?? 10;
+    if (timer == 0) {
+      // Dispose of the timerProvider when it reaches 0
+      ref.invalidate(timerProvider);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bus Arrivals'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(
             height: 10,
           ),
           BusStopCardWithFetch(busStopCode: busStopCode),
           const BusLoadLegend(),
+          const SizedBox(
+            height: 10,
+          ),
+          Text('Refresh in: $timer'),
+          const SizedBox(
+            height: 10,
+          ),
           BusArrivalList(busStopCode: busStopCode),
         ],
       ),
