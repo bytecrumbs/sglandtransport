258667f6cd037baa24849978f2e7c9cd3516f017
Sascha Derungs <sascha.derungs@gmail.com>
2025-06-06 20:59:47 +0800

    Update LTA Datamall API URL to use HTTPS and increment version number to 20.0.0

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,3 +1,3 @@
-const ltaDatamallApi = 'http://datamall2.mytransport.sg/ltaodataservice';
+const ltaDatamallApi = 'https://datamall2.mytransport.sg/ltaodataservice';
 const ltaDatamallApiKeyName = 'LTA_DATAMALL_API_KEY';
 const buildNameEnv = 'BUILD_NAME';
